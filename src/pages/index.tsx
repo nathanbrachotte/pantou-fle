08275f1fad25e5d6364df8506231be8211932562
Nathan Brachotte <nathan.brachotte+kraken@gmail.com>
2021-12-08 22:01:46 +0100

    add iframe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image } from 'rebass'
 import { graphql, StaticQuery } from 'gatsby'
 
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 import Landing from '../old-components/screens/Landing/Landing'
 import About from '../old-components/screens/About'
 import Projects from '../old-components/screens/Projects/Projects'
 import BlogPosts from '../old-components/screens/BlogPosts/BlogPosts'
 import Writing from '../old-components/screens/Writing/Writing'
-import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import { allExerciseSheetQuery } from '../api/queries'
 import SEO from '../components/SEO'
@@ -35,13 +35,30 @@ export const query = graphql`
   }
 `
 
-const IndexPage: React.FC = ({ data }: any) => {
-  console.log({ allExerciseSheetQuery })
+const IFrameWrapper = styled.div`
+  /* height: 0.7 vh; */
+  min-height: 675px;
+`
+const IFrame = styled.iframe`
+  width: 100%;
+  min-height: 675px;
+`
 
+const IndexPage: React.FC = ({ data }: any) => {
   return (
     <Layout>
       <SEO title="Blog" />
       {/* <Header /> */}
+      <IFrameWrapper>
+        <IFrame
+          title="iframe"
+          src="https://view.genial.ly/61b0ac256b7b2f0d4c0e7ed1"
+          allowFullScreen={false}
+          scrolling="yes"
+          frameBorder="0"
+        />
+      </IFrameWrapper>
+
       <iframe
         title={data.allContentfulFicheExercice.nodes[0].title}
         src={`${data.allContentfulFicheExercice.nodes[0].pdf.file.url}#toolbar=0`}
