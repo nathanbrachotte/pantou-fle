8922c439c4d5d326ab45984854ebe7b1c99d591f
N8 <nathan.brachotte+github@gmail.com>
2024-11-08 22:59:50 +0100

    fixup! fix images warning

--- src/shared/ActivityTypeHeader.tsx
+++ src/shared/ActivityTypeHeader.tsx
@@ -7,7 +7,7 @@ interface ActivityTypeHeaderProps {
 
 const ActivityTypeHeader: React.FC<ActivityTypeHeaderProps> = ({ title }) => {
   return (
-    <div className="mx-auto text-center flex flex-row mb-2 md:pl-2 lg:pl-0">
+    <div className="mx-auto text-center flex flex-row mb-2 md:pl-2 lg:pl-0 mt-4">
       <div className="flex-1">
         <Heading1>{title}</Heading1>
       </div>
