b3ebbc6ff120979828ea7e87a9fb07e7c297a56c
N8 <nathan.brachotte+github@gmail.com>
2024-11-09 01:31:06 +0100

    fixup! add bottom page ads

--- src/components/Ads/AdSense.tsx
+++ src/components/Ads/AdSense.tsx
@@ -7,78 +7,78 @@ export const OverHeaderAdSense: React.FC<{ path: string }> = ({ path }) => {
   //     ;(window.adsbygoogle = window.adsbygoogle || []).push({})
   //   }
   // }, [path])
-  console.log('🚀 ~ displaying ad for path:', path)
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-client="ca-pub-2142870138777008"
-      data-ad-slot="5603770416"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
-  )
+  // console.log('🚀 ~ displaying ad for path:', path)
+  // return (
+  //   <ins
+  //     className="adsbygoogle"
+  //     style={{ display: 'block' }}
+  //     data-ad-client="ca-pub-2142870138777008"
+  //     data-ad-slot="5603770416"
+  //     data-ad-format="auto"
+  //     data-full-width-responsive="true"
+  //   />
+  // )
 }
 
 export const InBetweenActivitiesAd: React.FC<{ activityId: string }> = ({
   activityId,
 }) => {
   return null
-  const isInitialized = useRef(false)
+  // const isInitialized = useRef(false)
 
-  useEffect(() => {
-    try {
-      if (
-        typeof window !== 'undefined' &&
-        window.adsbygoogle &&
-        !isInitialized.current
-      ) {
-        ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-        isInitialized.current = true
-      }
-    } catch (error) {
-      console.error('AdSense error:', error)
-    }
-  }, [activityId])
+  // useEffect(() => {
+  //   try {
+  //     if (
+  //       typeof window !== 'undefined' &&
+  //       window.adsbygoogle &&
+  //       !isInitialized.current
+  //     ) {
+  //       ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  //       isInitialized.current = true
+  //     }
+  //   } catch (error) {
+  //     console.error('AdSense error:', error)
+  //   }
+  // }, [activityId])
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-format="fluid"
-      data-ad-layout-key="-6z+dr+1e-1m+57"
-      data-ad-client="ca-pub-2142870138777008"
-      data-ad-slot="7581801144"
-    />
-  )
+  // return (
+  //   <ins
+  //     className="adsbygoogle"
+  //     style={{ display: 'block' }}
+  //     data-ad-format="fluid"
+  //     data-ad-layout-key="-6z+dr+1e-1m+57"
+  //     data-ad-client="ca-pub-2142870138777008"
+  //     data-ad-slot="7581801144"
+  //   />
+  // )
 }
 
 export const BottomOfPageAdSense: React.FC<{ path: string }> = ({ path }) => {
   return null
-  const isInitialized = useRef(false)
+  // const isInitialized = useRef(false)
 
-  useEffect(() => {
-    try {
-      if (
-        typeof window !== 'undefined' &&
-        window.adsbygoogle &&
-        !isInitialized.current
-      ) {
-        ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-        isInitialized.current = true
-      }
-    } catch (error) {
-      console.error('AdSense error:', error)
-    }
-  }, [path])
+  // useEffect(() => {
+  //   try {
+  //     if (
+  //       typeof window !== 'undefined' &&
+  //       window.adsbygoogle &&
+  //       !isInitialized.current
+  //     ) {
+  //       ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  //       isInitialized.current = true
+  //     }
+  //   } catch (error) {
+  //     console.error('AdSense error:', error)
+  //   }
+  // }, [path])
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-format="autorelaxed"
-      data-ad-client="ca-pub-2142870138777008"
-      data-ad-slot="8497527064"
-    />
-  )
+  // return (
+  //   <ins
+  //     className="adsbygoogle"
+  //     style={{ display: 'block' }}
+  //     data-ad-format="autorelaxed"
+  //     data-ad-client="ca-pub-2142870138777008"
+  //     data-ad-slot="8497527064"
+  //   />
+  // )
 }
