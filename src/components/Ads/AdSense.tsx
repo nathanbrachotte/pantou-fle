5e870335e0b1cc03c7667aec64bfc7eed342ab04
N8 <nathan.brachotte+github@gmail.com>
2026-02-12 04:26:20 +0100

    fixup! ds

--- src/components/Ads/AdSense.tsx
+++ src/components/Ads/AdSense.tsx
@@ -7,56 +7,52 @@ interface AdProps {
 }
 
 export const OverWebsiteAdSense: React.FC<AdProps> = () => {
-  return null
-  // const adProps = useAdsense({
-  //   adSlot: '2882619175',
-  //   customStyle: {
-  //     height: '280px',
-  //     backgroundColor: '#f0f0f0',
-  //   },
-  // })
+  const adProps = useAdsense({
+    adSlot: '2882619175',
+    customStyle: {
+      height: '280px',
+      backgroundColor: '#f0f0f0',
+    },
+  })
 
-  // return (
-  //   <div className="w-full bg-primary-light">
-  //     <ins {...adProps} />
-  //   </div>
-  // )
+  return (
+    <div className="w-full bg-primary-light">
+      <ins {...adProps} />
+    </div>
+  )
 }
 
 export const OverHeaderAdSense: React.FC<AdProps> = () => {
-  return null
-  // const adProps = useAdsense({
-  //   adSlot: '5603770416',
-  // })
+  const adProps = useAdsense({
+    adSlot: '5603770416',
+  })
 
-  // return (
-  //   <div className="px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8 w-full">
-  //     <ins {...adProps} />
-  //   </div>
-  // )
+  return (
+    <div className="px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8 w-full">
+      <ins {...adProps} />
+    </div>
+  )
 }
 
 export const InBetweenActivitiesAd: React.FC<AdProps> = () => {
-  return null
-  // const adProps = useAdsense({
-  //   adSlot: '7581801144',
-  //   adFormat: 'fluid',
-  //   adLayoutKey: '-6z+dr+1e-1m+57',
-  // })
+  const adProps = useAdsense({
+    adSlot: '7581801144',
+    adFormat: 'fluid',
+    adLayoutKey: '-6z+dr+1e-1m+57',
+  })
 
-  // return <ins {...adProps} />
+  return <ins {...adProps} />
 }
 
 export const BottomOfPageAdSense: React.FC<AdProps> = () => {
-  return null
-  // const adProps = useAdsense({
-  //   adSlot: '8497527064',
-  //   isAutoRelaxed: true,
-  // })
+  const adProps = useAdsense({
+    adSlot: '8497527064',
+    isAutoRelaxed: true,
+  })
 
-  // return (
-  //   <div className="px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8 w-full">
-  //     <ins {...adProps} />
-  //   </div>
-  // )
+  return (
+    <div className="px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8 w-full">
+      <ins {...adProps} />
+    </div>
+  )
 }
